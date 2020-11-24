2bcc635183249c1f15143c19ee56c23ab6642bcf
Matheus Hudson de Souza <matheushudson2013@gmail.com>
2020-11-24 20:11:47 -0300

    Criando navegação
    Criando a navegação das telas splash, login, e home.

---
+++ app/src/main/java/com/kotlin/marvelgeek/Fragments/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.kotlin.marvelgeek.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.kotlin.marvelgeek.R
+import kotlinx.android.synthetic.main.activity_login.view.*
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        view.btnLoginVisitante.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment2)
+        }
+
+        return view
+
+    }
+
+}
