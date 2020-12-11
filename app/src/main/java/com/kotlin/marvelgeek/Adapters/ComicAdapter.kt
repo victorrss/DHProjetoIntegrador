17cd4119d7bda29e6ca84fe33fb8328fccc2024f
gLeobonacini <70539330+gLeobonacini@users.noreply.github.com>
2020-12-11 00:45:48 -0300

    Service comic, event e story

--- app/src/main/java/com/kotlin/marvelgeek/Adapters/ComicAdapter.kt
+++ app/src/main/java/com/kotlin/marvelgeek/Adapters/ComicAdapter.kt
@@ -1,35 +1,45 @@
 package com.kotlin.marvelgeek.Adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.kotlin.marvelgeek.model.Comic
 import com.kotlin.marvelgeek.R
+import com.kotlin.marvelgeek.models.Comic
+import com.squareup.picasso.Picasso
 
-class ComicAdapter(
-    private val comicList: ArrayList<Comic>,
-    val listener: onClickListenerComic
-) : RecyclerView.Adapter<ComicAdapter.ViewHolderComic>() {
+class ComicAdapter(val listener: onClickListenerComic) : RecyclerView.Adapter<ComicAdapter.ViewHolderComic>() {
+
+    var listComic = ArrayList<Comic>()
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): ViewHolderComic {
+    ): ComicAdapter.ViewHolderComic {
         return ViewHolderComic(
             LayoutInflater.from(parent.context).inflate(R.layout.item_comics, parent, false)
         )
     }
 
-    override fun onBindViewHolder(holder: ViewHolderComic, position: Int) {
-        var comic: Comic = comicList[position]
+    override fun onBindViewHolder(holder: ComicAdapter.ViewHolderComic, position: Int) {
+        var comic: com.kotlin.marvelgeek.models.Comic = listComic[position]
 
-        holder.comicIvImage.setImageResource(comic.image)
+        Picasso.get().load("${comic.thumbnail.path}.${comic.thumbnail.extension}")
+            .fit()
+            .into(holder.comicIvImage)
         holder.comicTvTitle.text = comic.title
     }
 
-    override fun getItemCount(): Int = comicList.size
+    override fun getItemCount(): Int = listComic.size
+
+    fun addListComic(list: ArrayList<com.kotlin.marvelgeek.models.Comic>){
+        Log.i("TAG3",list.toString())
+        listComic.addAll(list)
+        notifyDataSetChanged()
+    }
 
     interface onClickListenerComic {
         fun onClickComic(position: Int)
--- app/src/main/java/com/kotlin/marvelgeek/Fragments/CharacterFragment.kt
+++ app/src/main/java/com/kotlin/marvelgeek/Fragments/CharacterFragment.kt
@@ -1,10 +1,15 @@
 package com.kotlin.marvelgeek.Fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kotlin.marvelgeek.Adapters.ComicAdapter
@@ -16,94 +21,110 @@ import com.kotlin.marvelgeek.model.Event
 import com.kotlin.marvelgeek.model.Serie
 import com.kotlin.marvelgeek.model.Storie
 import com.kotlin.marvelgeek.R
+import com.kotlin.marvelgeek.models.Character
+import com.kotlin.marvelgeek.services.repository
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_character.view.*
 import kotlinx.android.synthetic.main.activity_home.view.abHome
 
-class CharacterFragment : Fragment(), ComicAdapter.onClickListenerComic, StorieAdapter.onClickListenerStorie, EventAdapter.onClickListenerEvent, SerieAdapter.onClickListenerSerie {
-    var comicList = getAllComics()
-    var storieList = getAllStories()
-    var eventList = getAllEvents()
-    var serieList = getAllSeries()
+class CharacterFragment : Fragment(), ComicAdapter.onClickListenerComic{
+    //StorieAdapter.onClickListenerStorie,
+    //EventAdapter.onClickListenerEvent,
+    //SerieAdapter.onClickListenerSerie {
+
+    val viewModel by viewModels<MainViewModel>{
+        object : ViewModelProvider.Factory{
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return MainViewModel(repository) as T
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         val view = inflater.inflate(R.layout.fragment_character, container, false)
-        var adapterComic = ComicAdapter(comicList, this)
-        var adapterStorie = StorieAdapter(storieList, this)
-        var adapterEvent = EventAdapter(eventList, this)
-        var adapterSerie = SerieAdapter(serieList, this)
-
-        view.chaActRvComics.adapter = adapterComic
-
-        view.chaActRvEvents.adapter = adapterEvent
-        view.chaActRvSeries.adapter = adapterSerie
+        var mBundle =  Bundle()
+        if(mBundle != null) {
+            mBundle = requireArguments()
+        }
+        var character =  mBundle.getSerializable("character") as Character
+        (activity as AppCompatActivity).supportActionBar?.setTitle(character.name)
 
 
+        Picasso.get().load("${character.thumbnail.path}.${character.thumbnail.extension}")
+            .fit()
+            .into(view.chaActIvHero)
 
+        view.chaActTvBio.text = character.description
+
+        var adapterComic = ComicAdapter(this)
         view.chaActRvComics.layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL , false)
-        view.chaActRvEvents.layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL , false)
-        view.chaActRvSeries.layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL , false)
 
-        view.abHome.setOnClickListener {
-            findNavController().navigate(R.id.action_characterFragment_to_favoriteFragment)
+        // Comic
+        character.comics.items.forEach {
+            var id = it.resourceURI.split("/")
+            //Atualizando os valores da lista
+            var error = viewModel.getComic(id[id.size-1].toLong())
+            if (error != null){
+                val builder = AlertDialog.Builder(context)
+                builder.setTitle("Server problem:")
+                builder.setIcon(R.drawable.ic_info)
+                builder.setMessage(error)
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
+            }
+        }
+
+        while(viewModel.viewModelScope.isActive);
+        viewModel.listComic.observe(viewLifecycleOwner){
+            adapterComic.addListComic(it)
         }
 
 
 
+        //var adapterStorie = StorieAdapter(storieList, this)
+        //var adapterEvent = EventAdapter(eventList, this)
+        //var adapterSerie = SerieAdapter(serieList, this)
+
+        //view.chaActRvComics.adapter = adapterComic
+        //view.chaActRvEvents.adapter = adapterEvent
+        //view.chaActRvSeries.adapter = adapterSerie
+
+
+        //view.chaActRvEvents.layoutManager = LinearLayoutManager(activity,
+        //LinearLayoutManager.HORIZONTAL , false)
+        //view.chaActRvSeries.layoutManager = LinearLayoutManager(activity,
+        //LinearLayoutManager.HORIZONTAL , false)
+
+        view.abHome.setNavigationOnClickListener {
+            findNavController().navigate(R.id.action_characterFragment_to_favoriteFragment)
+        }
+
+        view.fbQuiz.setOnClickListener {
+            findNavController().navigate(R.id.action_characterFragment_to_quizFragment)
+        }
         return view
     }
 
-    fun getAllComics() = arrayListOf(
-        Comic("Immortal Hulk", "a", R.drawable.immortal_hulk),
-        Comic("Immortal Hulk #37", "b", R.drawable.imortal_hulk_37),
-        Comic("Immortal Hulk #38", "c", R.drawable.imortal_hulk_38),
-        Comic("Immortal Hulk #39", "d", R.drawable.immortal_hulk_39),
-        Comic("Juggernaut", "e", R.drawable.juggernaut)
-    )
-
-    fun getAllStories() = arrayListOf(
-        Storie("Immortal Hulk #37", "b", R.drawable.imortal_hulk_37),
-        Storie("Immortal Hulk #38", "c", R.drawable.imortal_hulk_38),
-        Storie("Immortal Hulk #39", "d", R.drawable.immortal_hulk_39),
-        Storie("Immortal Hulk", "a", R.drawable.immortal_hulk),
-        Storie("Juggernaut", "e", R.drawable.juggernaut)
-    )
-
-    fun getAllEvents() = arrayListOf(
-        Event("Juggernaut", "e", R.drawable.juggernaut),
-        Event("Immortal Hulk", "a", R.drawable.immortal_hulk),
-        Event("Immortal Hulk #37", "b", R.drawable.imortal_hulk_37),
-        Event("Immortal Hulk #38", "c", R.drawable.imortal_hulk_38),
-        Event("Immortal Hulk #39", "d", R.drawable.immortal_hulk_39)
-    )
-
-    fun getAllSeries() = arrayListOf(
-        Serie("Immortal Hulk", "a", R.drawable.immortal_hulk),
-        Serie("Immortal Hulk #37", "b", R.drawable.imortal_hulk_37),
-        Serie("Immortal Hulk #38", "c", R.drawable.imortal_hulk_38),
-        Serie("Immortal Hulk #39", "d", R.drawable.immortal_hulk_39),
-        Serie("Juggernaut", "e", R.drawable.juggernaut)
-    )
-
     override fun onClickComic(position: Int) {
-        findNavController().navigate(R.id.action_characterFragment_to_historiaFragment)
+        //findNavController().navigate(R.id.action_characterFragment_to_historiaFragment)
     }
 
 
 
-    override fun onClickEvent(position: Int) {
-        findNavController().navigate(R.id.action_characterFragment_to_eventFragment)
-    }
-
-
-    override fun onClickSerie(position: Int) {
-        findNavController().navigate(R.id.action_characterFragment_to_serieFragment)
-    }
-
-    override fun onClickStorie(position: Int) {
-
-    }
+//    override fun onClickEvent(position: Int) {
+//        //findNavController().navigate(R.id.action_characterFragment_to_eventFragment)
+//    }
+//
+//
+//    override fun onClickSerie(position: Int) {
+//        //findNavController().navigate(R.id.action_characterFragment_to_serieFragment)
+//    }
+//
+//    override fun onClickStorie(position: Int) {
+//        //findNavController().navigate(R.id.action_characterFragment_to_serieFragment)
+//    }
 }
