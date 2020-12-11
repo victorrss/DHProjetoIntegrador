a278a129cf34db7fcb47d352883623552f9c39c0
Matheus Hudson de Souza <matheushudson2013@gmail.com>
2020-12-11 00:47:32 -0300

    SharedMainViewModel
    Modificando o viewModel para um ViewModel compartilhado.

--- app/src/main/java/com/kotlin/marvelgeek/ui/MainActivity.kt
+++ app/src/main/java/com/kotlin/marvelgeek/ui/MainActivity.kt
@@ -2,23 +2,30 @@ package com.kotlin.marvelgeek.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.ui.AppBarConfiguration
 import com.kotlin.marvelgeek.R
+import com.kotlin.marvelgeek.ViewModel.MainViewModel
+import com.kotlin.marvelgeek.ViewModel.MyViewModelFactory
+import com.kotlin.marvelgeek.services.repository
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private val viewModel: MainViewModel by viewModels {
+        MyViewModelFactory(repository)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
-
+        viewModel
+        viewModel.getCharacter(20, 2)
     }
 
-    override fun onResume() {
 
-        super.onResume()
-    }
 
 }
