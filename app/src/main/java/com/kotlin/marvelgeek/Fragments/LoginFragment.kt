0adc25928985c99f484a4ed4ebb406e461dd7970
Jonatas Eliel <plipma@hotmail.com>
2021-02-05 09:47:50 -0300

    correcoes nas configuracoes do login

--- app/src/main/java/com/kotlin/marvelgeek/Fragments/LoginFragment.kt
+++ app/src/main/java/com/kotlin/marvelgeek/Fragments/LoginFragment.kt
@@ -1,7 +1,5 @@
 package com.kotlin.marvelgeek.Fragments
 
-import android.R.attr
-import com.facebook.appevents.AppEventsLogger
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -11,11 +9,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.facebook.*
-import com.facebook.FacebookSdk.getApplicationContext
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -23,9 +18,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FacebookAuthProvider
-import com.google.firebase.auth.FirebaseAuth
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.*
 import com.kotlin.marvelgeek.R
+import kotlinx.android.synthetic.main.activity_host.*
 import kotlinx.android.synthetic.main.fragment_login.view.*
 
 
@@ -49,7 +45,7 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        FacebookSdk.sdkInitialize(context)
+//        FacebookSdk.sdkInitialize(context)
 
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_login, container, false)
@@ -59,6 +55,8 @@ class LoginFragment : Fragment() {
 
         // GOOGLE SIGN-IN --------------------------------------------------------------------------
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestProfile()
             .requestEmail()
             .build()
 
@@ -71,27 +69,26 @@ class LoginFragment : Fragment() {
 
         // FACEBOOK SIGN-IN ------------------------------------------------------------------------
         view.btnLoginFacebook.setOnClickListener {
-            LoginManager.getInstance().logInWithReadPermissions(this, listOf("email"))
-            LoginManager.getInstance().registerCallback(callbackManager,
+            LoginManager.getInstance().logInWithReadPermissions(
+                this, listOf(
+                    "email",
+                    "public_profile"
+                )
+            )
+            LoginManager.getInstance().registerCallback(
+                callbackManager,
                 object : FacebookCallback<LoginResult> {
                     override fun onSuccess(result: LoginResult?) {
-                        result?.let {
-                            val token = it.accessToken
-                            val credential = FacebookAuthProvider.getCredential(token.token)
-
-                            FirebaseAuth.getInstance().signInWithCredential(credential)
-                                .addOnCompleteListener {
-                                    if (it.isSuccessful) {
-                                        Log.i("TAG", "Login com sucesso")
-                                        findNavController().navigate(R.id.action_loginFragment_to_homeFragment2)
-                                    } else {
-                                        Log.i("TAG", "Login falho")
-                                    }
-                                }
-                        }
+                        handleSignInResultFacebook(result!!.accessToken)
                     }
-                    override fun onCancel() {}
-                    override fun onError(error: FacebookException?) {}
+
+                    override fun onCancel() {
+                        Log.d("Debugando", "facebook:onCancel");
+                    }
+
+                    override fun onError(error: FacebookException?) {
+                        Log.d("Debugando", "facebook:onError", error);
+                    }
                 })
         }
 
@@ -106,11 +103,11 @@ class LoginFragment : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        // FACEBOOK
         callbackManager.onActivityResult(requestCode, resultCode, data)
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode === RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach a listener.
+        // GOOGLE
+        if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             handleSignInResultGoogle(task)
         }
@@ -119,18 +116,47 @@ class LoginFragment : Fragment() {
     private fun handleSignInResultGoogle(completedTask: Task<GoogleSignInAccount>) {
         try {
             val userGoogle = completedTask.getResult(ApiException::class.java)
+            val credential = GoogleAuthProvider.getCredential(userGoogle?.idToken, null)
 
-            // Signed in successfully, show authenticated UI.
-            if (userGoogle != null) {
-                findNavController().navigate(R.id.action_loginFragment_to_homeFragment2)
-            }
+            auth.signInWithCredential(credential)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d("Debugando", "signInWithCredential:success")
+                        val user: FirebaseUser? = auth.currentUser
+                        findNavController().navigate(R.id.action_loginFragment_to_homeFragment2)
+                    }
+                }.addOnFailureListener {
+                    Log.w("Debugando", "signInWithCredential:failure", completedTask.exception)
+                    Toast.makeText(
+                        activity, "Authentication failed. + ${completedTask.exception}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
         } catch (e: ApiException) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Toast.makeText(
-                activity, "Authentication failed.",
+                activity, "Authentication failed. + ${e.statusCode}",
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    private fun handleSignInResultFacebook(token: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(token.token)
+
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.i("TAG", "Login com sucesso")
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment2)
+                } else {
+                    Log.i("TAG", "Login falho")
+                }
+            }.addOnFailureListener {
+                Log.w("Debugando", "signInWithCredential:failure", it.cause)
+                Toast.makeText(
+                    activity, "Authentication failed. + ${it.cause}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 }
